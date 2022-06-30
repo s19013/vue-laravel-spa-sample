05b14cb064a050b3d8affcf6966ac5ddcae83f15
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-30 22:33:55 +0900

    TaskListComponent設置

--- resources/js/vue/App.vue
+++ resources/js/vue/App.vue
@@ -1,13 +1,17 @@
 <template>
     <header-component></header-component>
+    <task-list-component></task-list-component>
 </template>
 
 <script>
-import HeaderComponent  from "./components/HeaderComponent.vue";
+import HeaderComponent   from "./components/HeaderComponent.vue";
+import TaskListComponent from "./components/task/TaskListComponent.vue";
+
 export default {
   name: 'App',
   components: {
-    HeaderComponent
+    HeaderComponent,
+    TaskListComponent
   },
   data() {
     return {
