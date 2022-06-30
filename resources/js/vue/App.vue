e7cd38b662e6acfe9f9ce1abf07974766befe0d0
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-30 20:56:54 +0900

    軽いお試し

---
+++ resources/js/vue/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <button @click="addCount">{{ count }} 回クリックしました！</button>
+</template>
+<script setup>
+
+import { ref } from 'vue'
+
+const count = ref(0)
+const addCount = () => {
+
+    count.value++
+
+}
+
+</script>
