3a43a0625110f3668b0c45cf52d1025aca7f2d76
知念英弥 <s19013@std.it-college.ac.jp>
2022-07-01 21:32:32 +0900

    ルーティング

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\TaskController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +16,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::prefix('tasks')->group(function () {
+    Route::get ('/',[TaskController::class,'index']);
+    Route::post('/',[TaskController::class,'store']);
+    Route::get ('/{task}',[TaskController::class,'show']);
+    Route::post ('/update',[TaskController::class,'update']);
+    Route::delete('{task}',[TaskController::class,'destroy']);
 });
+
