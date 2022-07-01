8fb21f29c08b998ca2c32da8d5a68d4d9d326e8e
知念英弥 <s19013@std.it-college.ac.jp>
2022-07-01 12:25:54 +0900

    マージン追加

--- resources/js/vue/components/task/taskEditComponent.vue
+++ resources/js/vue/components/task/taskEditComponent.vue
@@ -19,7 +19,7 @@
                         <label for="person-in-charge" class="col-sm-5 col-form-label">Person In Charge</label>
                         <input type="text" class="col-sm-9 form-control" id="person-in-charge">
                     </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" class="btn btn-primary my-2">Submit</button>
                 </form>
             </div>
         </div>
