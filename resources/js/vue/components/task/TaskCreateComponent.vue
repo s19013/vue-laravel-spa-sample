9e7532204e5454e2a4e3dd47b11fa46c8dbec92b
知念英弥 <s19013@std.it-college.ac.jp>
2022-07-01 12:10:14 +0900

    見た目微調整

--- resources/js/vue/components/task/TaskCreateComponent.vue
+++ resources/js/vue/components/task/TaskCreateComponent.vue
@@ -3,19 +3,19 @@
         <div class="row justify-content-center">
             <div class="col-sm-6">
                 <form>
-                    <div class="form-group row">
-                        <label for="title" class="col-sm-3 col-form-label">Title</label>
+                    <div class="row">
+                        <label for="title" class="col-sm-5 col-form-label">Title</label>
                         <input type="text" class="col-sm-9 form-control" id="title">
                     </div>
-                    <div class="form-group row">
-                        <label for="content" class="col-sm-3 col-form-label">Content</label>
+                    <div class="row">
+                        <label for="content" class="col-sm-5 col-form-label">Content</label>
                         <input type="text" class="col-sm-9 form-control" id="content">
                     </div>
-                    <div class="form-group row">
-                        <label for="person-in-charge" class="col-sm-3 col-form-label">Person In Charge</label>
+                    <div class="row">
+                        <label for="person-in-charge" class="col-sm-5 col-form-label">Person In Charge</label>
                         <input type="text" class="col-sm-9 form-control" id="person-in-charge">
                     </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" class="btn btn-primary my-2">Submit</button>
                 </form>
             </div>
         </div>
