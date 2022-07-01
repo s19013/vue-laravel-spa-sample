ba529effaa9cbde3f9f697a18adb0bcfd15829ce
知念英弥 <s19013@std.it-college.ac.jp>
2022-07-01 12:14:47 +0900

    見た目微調整

--- resources/js/vue/components/task/TaskShowComponent.vue
+++ resources/js/vue/components/task/TaskShowComponent.vue
@@ -3,20 +3,20 @@
         <div class="row justify-content-center">
             <div class="col-sm-6">
                 <form>
-                    <div class="form-group row border-bottom">
-                        <label for="id" class="col-sm-3 col-form-label">ID</label>
+                    <div class="row border-bottom">
+                        <label for="id" class="col-sm-5 col-form-label">ID</label>
                         <input type="text" class="col-sm-9 form-control-plaintext" readonly id="id" v-bind:value="taskId">
                     </div>
-                    <div class="form-group row border-bottom">
-                        <label for="title" class="col-sm-3 col-form-label">Title</label>
+                    <div class="row border-bottom">
+                        <label for="title" class="col-sm-5 col-form-label">Title</label>
                         <input type="text" class="col-sm-9 form-control-plaintext" readonly id="title" value="title title">
                     </div>
-                    <div class="form-group row border-bottom">
-                        <label for="content" class="col-sm-3 col-form-label">Content</label>
+                    <div class="row border-bottom">
+                        <label for="content" class="col-sm-5 col-form-label">Content</label>
                         <input type="text" class="col-sm-9 form-control-plaintext" readonly id="content" value="content content">
                     </div>
-                    <div class="form-group row border-bottom">
-                        <label for="person-in-charge" class="col-sm-3 col-form-label">Person In Charge</label>
+                    <div class="row border-bottom">
+                        <label for="person-in-charge" class="col-sm-5 col-form-label">Person In Charge</label>
                         <input type="text" class="col-sm-9 form-control-plaintext" readonly id="person-in-charge" value="Ichiro">
                     </div>
                 </form>
