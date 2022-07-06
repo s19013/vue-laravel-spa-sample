2e1e52d3352a95aafeb705b23855e9a9af9ecf50
知念英弥 <s19013@std.it-college.ac.jp>
2022-07-06 12:07:15 +0900

    larave,vueが使えるように設定

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue'
-import laravel from 'vite-plugin-laravel'
-// import laravel from 'laravel-vite-plugin';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
-        vue(),
-        laravel(),
+        laravel({
+            input: 'resources/js/app.js',
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
 });
-
-// [
-//     'resources/css/app.css',
-//     'resources/js/app.js',
-// ]
